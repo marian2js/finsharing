f0bf078ac4574ec46b1e4d0e6ff80d0127465b6c
marian2js <marianomdq1@gmail.com>
2020-02-08 16:41:14 -0300

    Send emails with AWS SES

--- backend/src/plugins/emails/setup.ts
+++ backend/src/plugins/emails/setup.ts
@@ -1,5 +1,14 @@
 import { EmailModule } from '@commun/emails'
+import aws from 'aws-sdk'
+import nodemailer from 'nodemailer'
 
 export default async function () {
-  await EmailModule.setup(require('./config.json'))
+  await EmailModule.setup({
+    ...require('./config.json'),
+    transporter: nodemailer.createTransport({
+      SES: new aws.SES({
+        apiVersion: '2010-12-01'
+      })
+    })
+  })
 }
