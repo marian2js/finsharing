04e53b800b5b905f69d153c6b520d2af5e584b92
marian2js <marianomdq1@gmail.com>
2020-02-12 12:11:34 -0300

    Add support for yahoo finance videos on markdown

--- frontend/components/MarkdownContent.tsx
+++ frontend/components/MarkdownContent.tsx
@@ -38,21 +38,34 @@ export const MarkdownContent = (props: Props) => {
 function InlineCode (props: MarkdownAbstractSyntaxTree) {
   if (props.value) {
     if (props.value.startsWith('youtube:')) {
-      const youtubeId = props.value.split(':')[1]
+      const youtubeId = props.value.split(':')[1].trim()
       return <Youtube id={youtubeId}/>
     }
     if (props.value.startsWith('twitter:')) {
-      const twitterId = props.value.split(':')[1]
+      const twitterId = props.value.split(':')[1].trim()
       return <TweetEmbed id={twitterId}/>
     }
     if (props.value.startsWith('facebook:')) {
-      const facebookId = encodeURIComponent(props.value.split(':')[1])
+      const facebookId = encodeURIComponent(props.value.split(':')[1].trim())
       return (
         <div className="center-block text-center">
           <iframe
-            src={`https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2F${facebookId}&width=600&show_text=false&appId=539147866833020&height=336`}
+            src={`https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2F${facebookId}&width=600&show_text=false&appId=873138313118981&height=336`}
             width="600" height="336" style={{ border: 'none', overflow: 'hidden' }} scrolling="no" frameBorder="0"
             allowTransparency={true} allow="encrypted-media" allowFullScreen={true}/>
+        </div>
+      )
+    }
+    if (props.value.startsWith('yahoofinancevideo:')) {
+      const id = props.value.split('yahoofinancevideo:')[1].trim()
+      const url = /^https:\/\/finance.yahoo.com\/video\//.test(id) ? id : `https://finance.yahoo.com/video/${id}`
+      return (
+        <div style={{ position: 'relative', width: '100%', height: 0, paddingBottom: '56.25%' }}>
+          <iframe style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
+                  width="100%" height="100%" scrolling='no' frameBorder='0'
+                  src={`${url}?format=embed&recommendations=false`}
+                  allowFullScreen={true} allowTransparency={true}
+                  allow='autoplay; fullscreen; encrypted-media'/>
         </div>
       )
     }
