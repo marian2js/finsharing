e746a831b5295b80177e64f66d21428adebbf131
marian2js <marianomdq1@gmail.com>
2020-03-07 18:04:39 -0300

    Add redis env variables to next config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,9 @@ require('dotenv').config()
 module.exports = {
   env: {
     GA_TRACKING_CODE: process.env.GA_TRACKING_CODE,
-    endpoint: process.env.ENDPOINT
+    endpoint: process.env.ENDPOINT,
+    REDIS_HOST: process.env.REDIS_HOST,
+    REDIS_PORT: process.env.REDIS_PORT,
   },
   webpack: (config, { isServer }) => {
     if (!isServer) {
